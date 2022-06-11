6a77d0422df73b7c4acd5eb2c6aaf4cd46f7ef6a
Alex Kuck <akuck@cloudflare.com>
2022-06-10 19:29:46 -0700

    moving data file to folder

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for site in https://account.docusign.com/ https://outlook.office365.com/ https://accounts.google.com/ https://apps.shopify.com/ https://icanhazip.com
 do
-(cat ~/id.txt | tr -d "\n"; echo -n ","; echo -n "$site,"; curl -L -w "@./curl-output-format.txt" -o /dev/null -s $site; echo -n ","; date +%s) >> ~/data.csv
+(cat ~/id.txt | tr -d "\n"; echo -n ","; echo -n "$site,"; curl -L -w "@./curl-output-format.txt" -o /dev/null -s $site; echo -n ","; date +%s) >> ~/data/data.csv
 done
